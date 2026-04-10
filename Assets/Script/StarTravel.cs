4f095099113c48a165230a7a2f7364caa333585c
sarah <sarahemmelin@gmail.com>
2026-04-10 12:33:00 +0200

    New star (copy of old, new texture + shader), added wobble to Main_Star

--- Assets/Script/StarTravel.cs
+++ Assets/Script/StarTravel.cs
@@ -5,12 +5,24 @@ public class StarTravel : MonoBehaviour
     public float travelSpeed = 5f;
     public bool isMoving = false;
 
+    [Header("Wobble Settings")]
+    public float wobbleSpeed = 2f;
+    public float wobbleAmount = 5f;
+
+    private float startY;
+
+    void Start()
+    {
+        startY = transform.position.y;
+    }
+
     void Update()
     {
         if (isMoving)
         {
-            // Moves the star forward along the Z-axis
             transform.Translate(Vector3.forward * travelSpeed * Time.deltaTime);
+            float yOffset = Mathf.Sin(Time.time * wobbleSpeed) * wobbleAmount;
+            transform.position = new Vector3(transform.position.x, startY + yOffset, transform.position.z);
         }
     }
 }
