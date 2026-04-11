35225ad2eea3f591e75b27662707e27fe9418f5b
sarah <sarahemmelin@gmail.com>
2026-04-12 01:13:17 +0200

    fixed textures in Blender and exported new ones (of Ceres), reduced number of vertices in Blender, fixed the particle system around the Big_Star, added gravity and a force so not everything would get sucked in immediately, and then added deletion upon contact. Big_Star only affects particles and debris from Ceres.

--- Assets/Script/NebulaFriction.cs
+++ Assets/Script/NebulaFriction.cs
@@ -8,7 +8,6 @@ public class NebulaFriction : MonoBehaviour
     {
         if (other.TryGetComponent<Rigidbody>(out Rigidbody rb))
         {
-            // Smoothly apply resistance based on velocity
             Vector3 resistance = -rb.linearVelocity * gasDensity * Time.fixedDeltaTime;
             rb.AddForce(resistance, ForceMode.Acceleration);
         }
